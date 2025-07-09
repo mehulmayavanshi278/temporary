f5807b6efa697185019a69c69f386e10dd1b055f
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2025-07-09 22:40:56 +0530

    second commit

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -2,7 +2,10 @@
 import { GoogleMap, Marker } from 'vue3-google-map'
 
 const center = { lat: 40.689247, lng: -74.044502 }
-const apiKey = 'AIzaSyDglblqg6uz0TFXU0hHQ6mEVhBJ9FliDOg'
+let apiKey = 'AIzaSyDglblqg6uz0TFXU0hHQ6mEVhBJ9FliDOg'
+// first commit
+apiKey = 'second commit'
+
 </script>
 
 <template>
